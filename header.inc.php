d06c797396fbe17025e78cf5b5403c0cacb0c555
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-02-23 00:38:53 +0000

    added TSX and chaged login flags from id to class

--- header.inc.php
+++ header.inc.php
@@ -2,10 +2,11 @@
     <nav class="navbar navbar-default" role="navigation">
       <div class="container-fluid">
 	    <div class="navbar-header">
+		  <a class="navbar-brand" href="http://transcriptorium.eu">TSX</a>
 		  <a class="navbar-brand" href="http://transcriptorium.eu">
 		   <img alt="Brand" src="./images/tS_logo.jpg" width="30"  />
 		 </a>
-			 <a class="navbar-brand" href="http://ec.europa.eu/research/index.cfm">
+		 <a class="navbar-brand" href="http://ec.europa.eu/research/index.cfm">
 		   <img alt="Brand" src="./images/flag_yellow_high.png"  />
 		 </a>
 	     </div>
@@ -14,11 +15,11 @@
 		<li <?php print $menu['desk']; print $menu['transcribe'];?>><a href="./desk">Desk</a></li>
 		<li <?php print $menu['userarea'];?>><a href="./userarea">User area</a></li>
 	    </ul>
-	    <div id="tsx-not-logged-in">
+	    <div class="tsx-not-logged-in">
  	   	<button type="button" class="btn btn-default navbar-btn pull-right" data-toggle="modal" data-target="#loginModal">Sign in</button>
 	   	<button type="button" class="btn btn-default navbar-btn pull-right" data-toggle="modal" data-target="#createAccountModal">Sign up</button>
 	    </div>
-	    <div id="tsx-logged-in"> 
+	    <div class="tsx-logged-in"> 
 	   	<button type="button" class="btn btn-default navbar-btn pull-right" id="tsx-sign-out">Sign out</button>
 	    </div>
       </div>
