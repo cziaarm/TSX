6309adc81285ee0b5bc0e9227343a3d2140151cc
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-03-17 11:06:30 -0700

    the tei buttons include

---
+++ tei-buttons.inc.php
@@ -0,0 +1,21 @@
+						<div id="tsx-tei-buttons" class="btn-toolbar" role="toolbar" aria-label="...">
+							<div class="btn-group" role="group" aria-label="TEI tools">
+<!--								<button id="tsx-tei-linebreak" type="button" class="btn btn-default" title="Pagebreak"><span class="icon icon-pagebreak"></span></button> -->
+								<button id="tsx-tei-head" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Heading"><span class="icon icon-heading"></span></button>
+								<button id="tsx-tei-p" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Paragraph">p</button>
+								<button id="tsx-tei-add" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Addition">[+]</button>
+								<button id="tsx-tei-del" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Deletion"><strike>str</strike></button>
+								<button id="tsx-tei-unclear" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Questionable">?</button>
+								<button id="tsx-tei-gap" type="button" class="btn btn-default btn-sm tsx-tei-insert" title="Illegible">[...]</button>
+								<button id="tsx-tei-note" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Mrginal note">note</button>
+								<button id="tsx-tei-underscore" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Underscore"><u>U</u></button>
+								<button id="tsx-tei-super" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Superscript">x<sup>2</sup></button>
+								<button id="tsx-tei-sic" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Unusual spelling">&lt;sic&gt;</button>
+								<button id="tsx-tei-foreign" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Foreign">fr</button>
+								<button id="tsx-tei-amp" type="button" class="btn btn-default btn-sm tsx-tei-insert" title="Ampersand">&amp;</button>
+								<button id="tsx-tei-longdash" type="button" class="btn btn-default btn-sm tsx-tei-insert" title="Long dash">-</button>
+								<button id="tsx-tei-comment" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Comment">&lt;!--</button>
+<!--								<button id="tsx-suggest" type="button" class="btn btn-default" data-toggle="popover" title="Suggestions for next word(s)" data-html="true" data-content="<img src='css/throbber.gif'/>"><span class="glyphicon glyphicon-question-sign"></span></button> -->
+						</div>
+						</div>
+
