114e9bfe13abd93f51c0b7fe6703d244a6b8a063
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-04-12 01:56:15 +0100

    v simple script to handle metrics and write them to csv

---
+++ metrics.php
@@ -0,0 +1,20 @@
+<?php
+
+$date = date("Ymd");
+$headers = array('session', 'label', 'timestamp', 'user_id', 'document_ref', 'page_ref', 'line_length', 'line_number', 'line_ref', 'text');
+$session = $_REQUEST["session"];
+$csv_path = "/www/benpro/sitedata/TSX_metrics/".$date."_".$session.".csv";
+$file = fopen($csv_path,"a");
+$data = array();
+if(!filesize($csv_path)) 
+	fputcsv($file, $headers);
+
+foreach($_REQUEST as $k => $v){
+	if(in_array($k, $headers)) $data[] = $v;
+}
+
+#error_log("Data: ".$data);
+fputcsv($file, $data);
+
+fclose($file);
+?>
