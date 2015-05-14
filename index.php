2f6348e2a0a1ded5e6e34735cad3107f8f18b09c
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-05-14 09:37:32 +0100

    New content for FP

--- index.php
+++ index.php
@@ -11,55 +11,36 @@ require("./head.inc.php");?>
    <div class="row">
   	<div class="col-md-3 col-md-push-9">
 	    <div class="panel panel-default" id="tsx-image">
-	<!--		<div class="panel-heading"><strong>Jeremy Bentham</strong></div>
-			<div class="panel-body"> -->
 				<img src="<?php print $TSX_root;?>/images/Bentham.jpg" class="img-rounded" width="100%"/>
 				<p>Jeremy Bentham</p>
-<!--			</div> -->
 	    </div> 
 	</div>
 
   	<div class="col-md-9 col-md-pull-3">
-	 <!--   <div class="panel panel-default">
-			<div class="panel-heading"><strong>Welcome to TSX!</strong></div>
-			<div class="panel-body"> -->
 		<div class="jumbotron">
-			<h1>Welcome to TSX!!</h1>
-			<p>By registering with TSX you can do two of exciting things: transcribe fascinating and valuable manuscripts written and composed by the philosopher and reformer, Jeremy Bentham (1748-1832), and be among the first volunteer transcribers to test modern Handwritten Text Recognition (HTR) technology.</p>
-			<p>TSX may appear familiar to participants in the award-winning Transcribe Bentham initiative, but introduces new and innovative functionalities beyond transcription.</p>
-			<p>Volunteers may choose to receive from the HTR engine a full transcript of a given manuscript image, and then correct this automated transcript against the original, or to take advantage of the interactive transcription mode, in which the HTR engine offers suggestions for a subsequent word or words as you type.</p>
-			<p>These methods of participation are more fully explained in our instructional material.</p>
-			<p>We hope that you will be interested in testing out this innovative technology, and welcome any questions or feedback - positive or negative - to <a href="mailto:transcribe.bentham@ucl.ac.uk">transcribe.bentham@ucl.ac.uk</a>.</p>
+			<h1>Welcome to TSX!</h1>
+			<p>By registering an account with TSX you can do two exciting things: transcribe fascinating and valuable manuscripts written and composed by the philosopher and reformer, Jeremy Bentham (1748—1832), and be among the first volunteer transcribers to use Handwritten Text Recognition (HTR) technology in their work.</p>
+			<p>TSX places you in full control of the HTR technology, and allows you to participate in several interconnected ways:</p>
+			<ul>
+				<li>transcribe manuscripts without any assistance from the HTR engine</li>
+				<li>request a full transcript from the HTR engine of a given image, and then correct the transcript</li>
+				<li>take advantage of interactive transcription in which, should you become stuck on a particular word or section of a manuscript, request suggestions from the HTR engine</li>
+			</ul>
+			<p>These methods of participation are more fully explained in our <a href="http://www.transcribe-bentham.da.ulcc.ac.uk/td/Getting_started_with_TSX">instructional material</a>.</p>
+
+			<p>We hope that you will be interested in testing out this innovative technology, and welcome any questions or feedback – positive or negative – about your experience of using TSX at <a href="mailto:transcribe.bentham@ucl.ac.uk">transcribe.bentham@ucl.ac.uk</a></p>
+
+			<p>You can also complete a short, <a href="https://opinio.ucl.ac.uk/s?s=37112">anonymous online survey</a>, which will help us to evaluate TSX more fully.</p>
+
+			<strong>Compatibility</strong>
+			<p>We recommend that you access TSX using Google’s <a href="http://www.google.com/chrome">Chrome</a> browser, running on Windows. TSX will run on Firefox, though some functionality will be unavailable (such as zooming in and out of images using the mousewheel).</p>
+ 
+			<p>There are known issues with running TSX in Internet Explorer, and on MacOS. Future development will ensure compatibility with major browsers and operating systems. Please do accept our apologies for any inconvenience which this current incompatibility may cause.</p>
+
 			<div class="well well-sm" style="text-align: center;">
 				<strong>To begin transcribing please <span class="tsx-not-logged-in"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createAccountModal">Sign up</button> or <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">Sign in</button> and </span>visit the <a href="<?php print $TSX_root;?>/desk"><button type="button" class="btn btn-primary" title="TSX Desk">TSX Desk</button></strong></a>
 			</div>
-  <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
 		</div>
-
-<!--
-				<p>By registering with TSX you can do two of exciting things: transcribe fascinating and valuable manuscripts written and composed by the philosopher and reformer, Jeremy Bentham (1748-1832), and be among the first volunteer transcribers to test modern Handwritten Text Recognition (HTR) technology.</p>
-				<p>TSX may appear familiar to participants in the award-winning Transcribe Bentham initiative, but introduces new and innovative functionalities beyond transcription.</p>
-				<p>Volunteers may choose to receive from the HTR engine a full transcript of a given manuscript image, and then correct this automated transcript against the original, or to take advantage of the interactive transcription mode, in which the HTR engine offers suggestions for a subsequent word or words as you type.</p>
-				<p>These methods of participation are more fully explained in our instructional material.</p>
-				<p>We hope that you will be interested in testing out this innovative technology, and welcome any questions or feedback - positive or negative - to <a href="mailto:transcribe.bentham@ucl.ac.uk">transcribe.bentham@ucl.ac.uk</a>.</p>
-				<div class="well well-sm" style="text-align: center;">
-					<strong>To begin transcribing please <span class="tsx-not-logged-in"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#createAccountModal">Sign up</button> or <button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginModal">Sign in</button> and </span>visit the <a href="<?php print $TSX_root;?>/desk"><button type="button" class="btn btn-default" title="TSX Desk">TSX Desk</button></strong></a>
-				</div>
--->
-<!--
-			</div>
-	    </div> -->
-<!--	</div> -->
-<!--  	<div class="col-md-9 col-md-push-3"> -->
-<!--	    <div class="panel panel-default">
-			<div class="panel-heading"><strong>Credits</strong></div>
-			<div class="panel-body">
-				<p>TSX was developed by the <a href="http://www.ulcc.ac.uk">University of London Computer Centre</a> and <a href="http://www.ucl.ac.uk">University College London</a> as part of the EU FP7-funded tranScriptorium consortium, which is developing innovative, efficient and cost-effective solutions for the indexing, searching, and full transcription of historic handwritten manuscript images, using modern, holistic HTR technology.</p>
-				<p>tranScriptorium is led by the <a href="http://www.upv.es">Universitat Politècnica de València</a>, and has received funding from the European Union’s Seventh Framework Programme for research, technological development and demonstration under grant agreement no 600707.</p></p>
-			</div>
-	    </div>
-	</div>
--->
 
 
    </div>
