e6bd1b585104db8816a5275cae960c9d354aeaf4
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-05-14 09:35:44 +0100

    add username to metrics

--- metrics.php
+++ metrics.php
@@ -1,7 +1,7 @@
 <?php
 
 $date = date("Ymd");
-$headers = array('session', 'label', 'timestamp', 'user_id', 'document_ref', 'page_ref', 'line_length', 'line_number', 'line_ref', 'text');
+$headers = array('session', 'label', 'timestamp', 'user_id', 'user_name', 'document_ref', 'page_ref', 'line_length', 'line_number', 'line_ref', 'text');
 $session = $_REQUEST["session"];
 $csv_path = "/www/benpro/sitedata/TSX_metrics/".$date."_".$session.".csv";
 $file = fopen($csv_path,"a");
