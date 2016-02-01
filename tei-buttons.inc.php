c453109a1be92961c8a166fdbf706bdf4b4a8293
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-02-01 01:02:09 +0000

    remove uneeded buttons

--- tei-buttons.inc.php
+++ tei-buttons.inc.php
@@ -1,7 +1,6 @@
 						<div id="tsx-tei-buttons" class="btn-toolbar" role="toolbar" aria-label="...">
 							<div class="btn-group" role="group" aria-label="TEI tools">
 								<button id="tsx-tei-head" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Heading"><span class="icon icon-heading"></span></button>
-								<button id="tsx-tei-p" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Paragraph">p</button>
 								<button id="tsx-tei-add" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Addition">[+]</button>
 								<button id="tsx-tei-del" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Deletion"><strike>str</strike></button>
 								<button id="tsx-tei-unclear" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Questionable">?</button>
@@ -11,9 +10,6 @@
 								<button id="tsx-tei-super" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Superscript">x<sup>2</sup></button>
 								<button id="tsx-tei-sic" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Unusual spelling">&lt;sic&gt;</button>
 								<button id="tsx-tei-foreign" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Foreign">fr</button>
-								<button id="tsx-tei-amp" type="button" class="btn btn-default btn-sm tsx-tei-insert" title="Ampersand">&amp;</button>
-								<button id="tsx-tei-longdash" type="button" class="btn btn-default btn-sm tsx-tei-insert" title="Long dash">-</button>
-								<button id="tsx-tei-comment" type="button" class="btn btn-default btn-sm tsx-tei-wrap" title="Comment">&lt;!--</button>
 						</div>
 						</div>
 
