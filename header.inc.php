98af0aaf20b126b13f0e7a3305982fb43e1eabaa
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-02-01 01:01:14 +0000

    doc-control in header

--- header.inc.php
+++ header.inc.php
@@ -24,11 +24,13 @@
 				<ul class="nav navbar-nav" id="tsx-salutation">
 				 <li><a href="./userarea"></a></li>
 				 </ul>
-
 	    <div class="tsx-logged-in"> 
 	   	<button type="button" class="btn btn-primary navbar-btn pull-right" id="tsx-sign-out">Sign out</button>
 		<!--<button type="button" class="btn btn-default navbar-btn pull-right" id="tsx-user-data">TSX user area</button>-->
 	    </div>
+	    <div class="tsx-doc-control"> 
+	    </div>
+
       </div>
     </nav>
 
